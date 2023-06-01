6e7fd517a98bd62b16b6b446cc0f0aaba8ea879e
ggghosthat <ildarildar990@gmail.com>
2023-06-01 11:35:01 +0300

    Extensions.cs refactored

--- Instances/Extensions/Extensions.cs
+++ Instances/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
 using ShareInstances.Instances.Interfaces;
+using ShareInstances.Instances;
 using ShareInstances.Filer;
 using ShareInstances.Services.Entities;
 
@@ -8,53 +9,39 @@ using System.Linq;
 namespace ShareInstances.Instances;
 public static class Extesions
 {
-    public static T ToEntity<T>(this Guid guid, IList<T> store) where T : ICoreEntity
+    //Search for the single item
+	public static Track ToTrackEntity(this Guid guid, IList<Track> store)
     {
         return store.ToList().FirstOrDefault(i => guid.Equals(i.Id));
     }
 
-	public static List<T> ToEntity<T>(this IList<Guid> guids, IList<T> store, bool full_return = false ) where T : ICoreEntity
+    public static Artist ToArtistEntity(this Guid guid, IList<Artist> store)
+    {
+        return store.ToList().FirstOrDefault(i => guid.Equals(i.Id));
+    }
+
+    public static Playlist ToArtistEntity(this Guid guid, IList<Playlist> store)
+    {
+        return store.ToList().FirstOrDefault(i => guid.Equals(i.Id));
+    }
+
+
+    //Search for list of entities in the input store
+    public static List<Track> ToTrackEntities(this IList<Guid> guids, IList<Track> store)
 	{
-        if (!full_return)
-    		return store.ToList()
-                        .FindAll(delegate(T item) { return guids.Contains(item.Id); });
-        else
-        {
-            return store.ToList();   
-        }
+    	return store.ToList()
+                    .FindAll(delegate(Track item) { return guids.Contains(item.Id); });
 	}
 
-    public static Track MusicFileConvertTrack(this MusicFile musicFile)
-    {
-        string picture64Base = null;
-        if (musicFile.Picture is not null)
-        {
-            picture64Base = Convert.ToBase64String(musicFile.Picture);
-        }
-        return new Track(musicFile.FilePath,
-                     musicFile.FileName,
-                     "",
-                     picture64Base);
-    }
+    public static List<Artist> ToTrackEntities(this IList<Guid> guids, IList<Artist> store)
+	{
+    	return store.ToList()
+                    .FindAll(delegate(Artist item) { return guids.Contains(item.Id); });
+	}
 
-
-    //Here supporter null value for "temporary playing".
-    //And we need pass supporter when we wanna save temporary instances (MusicFile).
-    public static Playlist MusicFileConvertPlaylist(this IEnumerable<MusicFile> musicFiles,
-                                                         SupporterService supporter = null)
-    {
-        //playlist initialization
-        var playlist = new Playlist(name:DateTime.Now.ToString(), 
-                                    description:"");
-
-        //tracks initialization & playlist population
-        musicFiles.ToList().ForEach(mf => 
-        {
-            var track = mf.MusicFileConvertTrack();
-            if (supporter is not null) supporter.AddInstance(track);
-            playlist.AddTemp(track);
-        });   
-
-        return playlist;
-    }
+    public static List<Playlist> ToTrackEntities(this IList<Guid> guids, IList<Playlist> store)
+	{
+    	return store.ToList()
+                    .FindAll(delegate(Playlist item) { return guids.Contains(item.Id); });
+	}
 }
