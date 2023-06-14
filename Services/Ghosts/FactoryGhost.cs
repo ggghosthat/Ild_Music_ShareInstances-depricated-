9f0203dd29c59a5175c7b63649460d718ce2cea1
ggghosthat <ildarildar990@gmail.com>
2023-06-14 10:31:41 +0300

    new track initialiazing process

--- Services/Ghosts/FactoryGhost.cs
+++ Services/Ghosts/FactoryGhost.cs
@@ -1,10 +1,13 @@
 using ShareInstances.Instances;
+using ShareInstances.Filer;
 using ShareInstances.Services.Entities;
 using ShareInstances.Services.Interfaces;
 using ShareInstances.Services.InstanceProducer;
 using ShareInstances.Exceptions.SynchAreaExceptions;
 
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 
 namespace ShareInstances.Services.Entities;
@@ -65,25 +68,55 @@ public class FactoryGhost : IGhost
     }
 
     public void CreateTrack(string pathway,
-                            string name,
-                            string description,
-                            string avatar = null,
+                            string name=null,
+                            string description=null,
+                            string avatarPath = null,
                             IList<Artist> artists = null)
     {      
         try
-        {      
-            producer = new InstanceProducer.InstanceProducer(pathway.AsMemory(),
-                                                             name.AsMemory(),
-                                                             description.AsMemory(),
-                                                             avatar.AsMemory(),
-                                                             artists);
-            SupportGhost.AddTrackInstance(producer.TrackInstance);
-            producer.Dispose();
+        {               
+            using(var taglib = TagLib.File.Create(pathway))
+            {
+                
+                Memory<char> trackName = taglib.Tag.Title.ToCharArray()
+                                    ?? Path.GetFileName(pathway).ToCharArray();
+                Memory<char> trackDescription = description.ToCharArray();
+
+
+                Memory<byte> trackAvatarSource;
+                if (string.IsNullOrEmpty(avatarPath))
+                {
+                    if(taglib.Tag.Pictures.Length > 0)
+                    {
+                         trackAvatarSource = taglib.Tag.Pictures[0].Data.Data;
+                    }
+                }
+                else 
+                {
+                    trackAvatarSource = ExtractTrackAvatar(avatarPath.AsMemory()).Result;
+                }
+
+                producer = new InstanceProducer.InstanceProducer(pathway.AsMemory(),
+                                                                 name.AsMemory(),
+                                                                 description.AsMemory(),
+                                                                 avatarPath.AsMemory(),
+                                                                 artists);
+                SupportGhost.AddTrackInstance(producer.TrackInstance);
+                producer.Dispose();
+            }
         }
         catch (InvalidTrackException ex)
         {
             throw ex;
         }
+    }
+    #endregion
+
+    #region Accessory Methods
+
+    private async ValueTask<Memory<byte>> ExtractTrackAvatar(ReadOnlyMemory<char> pathway)
+    {
+        return await ShareInstances.Filer.Filer.ReadFileAsync(pathway.ToString());
     }
     #endregion
 }
